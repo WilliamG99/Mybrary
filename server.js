316ed48d47a5c2b003e969c5dc3336220ceee506
WilliamG99 <19willgarcia99@gmail.com>
2023-04-21 16:40:01 +0100

    Books create index new

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-};
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser');
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
-const indexRouter = require('./routes/index');
-const authorRouter = require('./routes/authors');
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
-
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-app.use(express.static('/public'));
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
-db.on('error', error => console.error('connection error:'));
-db.once('open', () => console.error('Connection successful!'));
+db.on("error", (error) => console.error("connection error:"));
+db.once("open", () => console.error("Connection successful!"));
 
-
-
-app.use('/', indexRouter);
-app.use('/authors', authorRouter);
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 app.listen(process.env.PORT || 3000);
